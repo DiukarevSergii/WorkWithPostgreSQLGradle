79f44bbaa5c1c4b09b92a83f6de4e869c281e1aa
Diukarev Sergii <vmesteprotiv@gmail.com>
2016-12-08 13:26:54 +0200

    add mysql

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -11,11 +11,13 @@ public class HelloWorld {
         Statement stmt = null;
         try {
             Class.forName("org.postgresql.Driver");
+//            Class.forName("com.mysql.cj.jdbc.Driver");
             c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/newdb", "postgres", "password");
+//            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false", "root", "666999");
             System.out.println("Opened database successfully");
 
             stmt = c.createStatement();
-            String sql = "CREATE TABLE COMPANY " +
+            String sql = "CREATE TABLE IF NOT EXISTS COMPANY " +
                     "(ID INT PRIMARY KEY     NOT NULL," +
                     " NAME           TEXT    NOT NULL, " +
                     " AGE            INT     NOT NULL, " +
